250a217c0201739b69d4e85e3f429439a8e96b8a
Shion Hirayama <hirayama@plusv.jp>
2024-12-03 11:15:31 +0900

    コメントはなし

--- kadai9.go
+++ kadai9.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 func main() {
 
-	slice := []string{"あいう", "あいうえ", "あいうえお", "かきくけこ", "かきく"}
-	for _, str := range slice {
-		//fmt.Println(str, "len:", utf8.RuneCountInString(str))
-		fmt.Println(max(str))
-	}
+	str := []string{"あいう", "あいうえ", "あいうえお", "かきくけこ", "かきく"}
+	//for _, str := range slice {
+	//fmt.Println(str, "len:", utf8.RuneCountInString(str))
+	fmt.Println(slices.Max(str))
+	//}
 
 }
